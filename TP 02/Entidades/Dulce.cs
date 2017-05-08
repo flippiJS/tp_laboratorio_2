19434db4c37b47fc5e28e95668dd3fa33afa9727
soyflippi <soyflippi@gmail.com>
2017-05-08 12:05:42 -0300

    Subida TP02

---
+++ TP 02/Entidades/Dulce.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Dulce : Producto
+    {
+        #region "Constructor"
+        /// <summary>
+        /// Constructor que llama a la clase base (padre) pasandole los argumentos
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Dulce(EMarca marca, string patente, ConsoleColor color) 
+            : base(marca, patente, color) { }
+        #endregion
+
+        #region "Propiedades"
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get{return 80;}
+        }
+        #endregion
+
+        #region "Métodos"
+        /// <summary>
+        /// Devuelve las características del objeto
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ TP 02/Entidades/Leche.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Entidades_2017
+{
+    public class Leche : Producto
+    {
+        #region "Atributos"
+        public enum ETipo { Entera, Descremada }
+
+        ETipo _tipo;
+        #endregion
+
+        #region "Constructores"
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca">Marca del producto</param>
+        /// <param name="codigoDeBarras">Código de barras del producto</param>
+        /// <param name="color">Color del empaque del producto</param>
+        public Leche(EMarca marca, string codigoDeBarras, ConsoleColor color)
+            : base(marca, codigoDeBarras, color)
+        {
+            _tipo = ETipo.Entera;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="marca">Marca del producto</param>
+        /// <param name="codigoDeBarras">Código de barras del producto</param>
+        /// <param name="colorPrimarioEmpaque">Color del empaque del producto</param>
+        /// <param name="tipo">Tipo específico del producto</param>
+        public Leche(EMarca marca, string codigoDeBarras, ConsoleColor colorPrimarioEmpaque, ETipo tipo)
+            : this(marca, codigoDeBarras, colorPrimarioEmpaque)
+        {
+            this._tipo = tipo;
+        }
+
+        #endregion
+
+        #region "Propiedades"
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get{return 20;}
+        }
+
+        #endregion
+
+        #region "Metodos"
+        /// <summary>
+        /// Devuelve las características del objeto
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
+            sb.AppendLine("TIPO : " + this._tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
