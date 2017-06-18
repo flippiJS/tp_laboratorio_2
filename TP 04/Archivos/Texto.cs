6b16a54807f612f275b36db4a864de5156a31172
Flippi <soyflippi@gmail.com>
2017-06-18 19:01:08 -0300

    Subida TP 4

---
+++ TP 04/Archivos/Texto.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        #region Atributos
+
+        private string _file;
+
+        #endregion
+
+        #region Metodos
+
+        public Texto(string data)
+        {
+            this._file = data;
+        }
+
+        public bool guardar(string data)
+        {
+            bool retorno;
+            StreamWriter txt = new StreamWriter(this._file, true);
+
+            try
+            {
+                txt.WriteLine(data);
+                retorno = true;
+            }
+            catch (IOException e)
+            {
+                Console.Write("Error al guardar: " + this._file + "\x0D\x0A" + e.StackTrace);
+                retorno = false;
+            }
+            finally
+            {
+                txt.Close();
+            }
+
+            return retorno;
+        }
+
+        public bool leer(out List<String> data)
+        {
+            bool retorno;
+
+            try
+            {
+                var file = File.ReadAllLines(this._file);
+                data = new List<String>(file);
+                retorno = true;
+            }
+            catch (Exception e)
+            {
+                Console.Write("Error al leer: " + this._file + "\x0D\x0A" + e.Message);
+                data = new List<String>();
+                retorno = false;
+            }
+
+            return retorno;
+        }
+
+        #endregion
+    }
+}
---
+++ TP 04/Hilo/Descargador.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+
+    public class Descargador
+    {
+        #region Atributos
+
+        private string html;
+        private Uri direccion;
+
+        #endregion
+
+        #region Delegados
+
+        public delegate void progresoCarga(int progreso);
+        public delegate void descargaCompleta(string resultado);
+
+        #endregion
+
+        #region Eventos
+
+        public event progresoCarga progreso;
+        public event descargaCompleta descargaFinalizada;
+
+        #endregion
+
+        #region Metodos
+
+        public Descargador(Uri direccion)
+        {
+            this.direccion = direccion;
+            this.html = "";
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted;
+
+                cliente.DownloadStringAsync(this.direccion);
+            }
+            catch (Exception e)
+            {
+                Console.Write("Error: " + e.Message);
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.progreso(e.ProgressPercentage);
+        }
+
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                this.html = e.Result;
+                this.descargaFinalizada(this.html);
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error al intentar acceder a URL solicitada: "+err.Message);               
+            }
+        }
+
+        #endregion
+    }
+}
